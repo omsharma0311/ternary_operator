30c34de1f4f65c780c4d02352e5aeeb73aff0e12
OM SHARMA <Osharma311@gmail.com>
2019-01-18 13:01:04 +0530

    Add files via upload
    Implementation of ternary operator in c using even odd program

---
+++ number.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+void main()
+
+{
+    
+	int num;
+   
+	printf("Enter a no \n");
+   
+ 	scanf("%d",&num);
+    
+	(num%2==0)?printf("num is even"):printf("numis odd");
+    
+	return 0;
+   
+}
